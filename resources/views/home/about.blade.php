3bcb19bb96fc8fcb3c98743e325d9d571cf7d415
tubabugru <tubabugru19@gmail.com>
2021-12-11 23:38:56 +0300

    Index Changed

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.home')
+
+@section('title', 'About us')
+
+@section('content')
+    <div class="col-sm-12">
+    <h1>About Us</h1>
+    aaaaaaaaaaaaaaa
+    <br>
+    jjjjjjjjjjjjjjjjjjjjj <br>
+    llllllllllllllllllll <br>
+    ppppppppppppppppppp <br>
+    <br>
+    </div>
+@endsection
